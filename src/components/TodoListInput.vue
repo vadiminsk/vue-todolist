ebb3466cf70103f5d2150b58a763cdd27550267b
Vadim <vadiminsk90@gmail.com>
2021-10-01 15:16:25 +0300

    add components

---
+++ src/components/TodoListInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mb-4">
+    <label for="todo-text" class="block text-sm font-medium text-gray-700"
+      ><input
+        type="text"
+        name="price"
+        id="todo-text"
+        class="focus:ring-indigo-500 focus:border-indigo-500 block w-full p-3 pl-5 sm:text-sm border border-gray-300 rounded-md"
+        placeholder="What needs to be done?"
+    /></label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoListInput",
+};
+</script>
+
+<style></style>
